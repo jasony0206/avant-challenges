0f1a69d305215455bbf5a2d3a0c70e06f58c5d53
jasony0206 <jasony0206@gmail.com>
2016-01-27 22:26:13 -0800

    Comment getFactorsDP

--- src/Factors.java
+++ src/Factors.java
@@ -7,7 +7,7 @@ public class Factors {
             list.add(i);
         }
 
-        Map<Integer, ArrayList<Integer>> map = getFactors(list);
+        Map<Integer, ArrayList<Integer>> map = getFactorsDP(list);
         System.out.println(map);
     }
 
@@ -30,34 +30,59 @@ public class Factors {
         return map;
     }
 
+    // Dynamic programming implementation of getFactors method.
+    // Given an input of size 100000, it runs about 10 times faster than the naive approach.
+    // General idea: This is a bottom-up DP algorithm. Consider the following example.
+    // [2, 5, 10, 20]
+    // When we are about to process 20, we already know the factors of 2, 5, and 10.
+    // We can leverage memoization technique here to reduce time complexity.
+    // More details below.
     public static Map<Integer, ArrayList<Integer>> getFactorsDP(ArrayList<Integer> inputList) {
         Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+
+        // First, sort the input list to reduce the traversal scope for each number:
+        // When looking for factors of a given number, we only need to consider numbers to the left of it.
         Collections.sort(inputList);
         int length = inputList.size();
 
         for (int i = 0; i < length; i++) {
-            Set<Integer> curFactorsSet = new HashSet<Integer>();
             int curNum = inputList.get(i);
-            int smallestCounterpart = Integer.MAX_VALUE;
+            Set<Integer> curFactorsSet = new HashSet<Integer>();
 
+            // 'marker' is a crucial part of this DP algorithm.
+            // It is used in a similar fashion to how Quick Sort's partition function uses two counters.
+            // The 'other' variable below acts as the left counter, and
+            // 'marker' acts as the right counter.
+            // When 'other' becomes greater than 'marker' is when we can stop.
+            int marker = Integer.MAX_VALUE;
+
+            // Only consider numbers to the left of curNum.
             for (int j = 0; j < i; j++) {
                 int other = inputList.get(j);
-                if (other > smallestCounterpart) {
+                // 'other' greater than 'marker', finished work for this 'curNum'.
+                if (other > marker) {
                     break;
                 }
 
                 if (curNum % other == 0) {
+                    // Use pre-computed result: we already know factors of 'other'
                     Set<Integer> memoizedFactorSet1 = new HashSet<Integer>(map.get(other));
-                    ArrayList<Integer> memoizedFactorsList2 = map.get(curNum / other);
                     curFactorsSet.addAll(memoizedFactorSet1);
                     curFactorsSet.add(other);
 
+                    ArrayList<Integer> memoizedFactorsList2 = map.get(curNum / other);
                     if (memoizedFactorsList2 != null) {
+                        // Use pre-computed result: we already know factors of 'curNum / other'
+                        // which is also a factor of curNum.
                         Set<Integer> memoizedFactorSet2 = new HashSet<Integer>(map.get(curNum / other));
                         curFactorsSet.addAll(memoizedFactorSet2);
                         curFactorsSet.add(curNum / other);
                     }
-                    smallestCounterpart = curNum / other;
+
+                    // 'marker' marks the smallest value of 'curNum / other' so far.
+                    // When this number becomes smaller than 'other', we're done working for
+                    // this particular 'curNum'
+                    marker = curNum / other;
                 }
             }
 
