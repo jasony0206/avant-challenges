fed40e7896ce04c4420c6e30ebb6352e7e707e61
jasony0206 <jasony0206@gmail.com>
2016-01-27 21:21:58 -0800

    Modify getFactors's interface

--- src/Factors.java
+++ src/Factors.java
@@ -4,25 +4,27 @@ import java.util.Map;
 
 public class Factors {
     public static void main(String[] args) {
-
-    }
-
-    public static void getFactors() {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 1; i < 100000; i++) {
+        for (int i = 1; i < 25; i++) {
             list.add(i);
         }
 
+        Map<Integer, ArrayList<Integer>> map = getFactors(list);
+        System.out.println(map);
+    }
+
+    public static Map<Integer, ArrayList<Integer>> getFactors(ArrayList<Integer> inputList) {
         Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
 
-        for (Integer num : list) {
+        for (Integer num : inputList) {
             ArrayList<Integer> tempList = new ArrayList<Integer>();
-            for (Integer other : list) {
+            for (Integer other : inputList) {
                 if (num % other == 0) {
                     tempList.add(other);
                 }
             }
             map.put(num, tempList);
         }
+        return map;
     }
 }
