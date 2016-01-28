a1ab62b2b4b58d853bb30ce2a9d91d85b22d7df0
jasony0206 <jasony0206@gmail.com>
2016-01-27 21:33:00 -0800

    Start working on DP version of getFactors

--- src/Factors.java
+++ src/Factors.java
@@ -1,15 +1,13 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Factors {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 1; i < 25; i++) {
+        for (int i = 1; i < 13; i++) {
             list.add(i);
         }
 
-        Map<Integer, ArrayList<Integer>> map = getFactors(list);
+        Map<Integer, ArrayList<Integer>> map = getFactorsDP(list);
         System.out.println(map);
     }
 
@@ -17,13 +15,36 @@ public class Factors {
         Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
 
         for (Integer num : inputList) {
-            ArrayList<Integer> tempList = new ArrayList<Integer>();
+            ArrayList<Integer> curFactorsList = new ArrayList<Integer>();
             for (Integer other : inputList) {
                 if (num % other == 0) {
-                    tempList.add(other);
+                    curFactorsList.add(other);
                 }
             }
-            map.put(num, tempList);
+            map.put(num, curFactorsList);
+        }
+        return map;
+    }
+
+    public static Map<Integer, ArrayList<Integer>> getFactorsDP(ArrayList<Integer> inputList) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        Collections.sort(inputList);
+        int length = inputList.size();
+
+        for (int i = 0; i < length; i++) {
+            Set<Integer> curFactorsSet = new HashSet<Integer>();
+            int curNum = inputList.get(i);
+
+            for (int j = 0; j < i; j++) {
+                int other = inputList.get(j);
+                if (curNum % other == 0) {
+                    curFactorsSet.add(other);
+                }
+            }
+
+            ArrayList<Integer> curFactorsList = new ArrayList<Integer>();
+            curFactorsList.addAll(curFactorsSet);
+            map.put(curNum, curFactorsList);
         }
         return map;
     }
