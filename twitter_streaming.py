91fed14c41fb674c474be35f3c095a9a11275e5f
jasony0206 <jasony0206@gmail.com>
2016-01-30 17:37:26 -0800

    Get top 10 most frequent words

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,5 +1,6 @@
 import tweepy
 import stop_words as sw
+import operator
 
 access_token = "4856028904-WRZgbA4C2V6kLHKaNNFP4ag75cQc3QMVceySBYy"
 access_token_secret = "yfVs5cwz8TiooQbBeEAJKzAB0Ws4sYVvKvtFtDpoW6f3i"
@@ -29,19 +30,34 @@ class TweetStreamListener(tweepy.StreamListener):
         	return False
 
 if __name__ == '__main__':
-    # listner = TweetStreamListener()
-    # stream = tweepy.streaming.Stream(auth, listner)
-    # stream.filter(languages=['en'], locations=[-180, -90, 180, 90 ])
+    listner = TweetStreamListener()
+    stream = tweepy.streaming.Stream(auth, listner)
+    stream.filter(languages=['en'], locations=[-180, -90, 180, 90 ])
     
     # parse the file into words
     with open('tweets.txt', 'r') as tweets:
     	tweets_string = tweets.read().replace('\n', '')
-    	tweets_words = tweets_string.split(' ')
-    	tweets_wordcount = len(tweets_words)
+    	# normalize by turning all words to lowercase
+    	tweets_words = [word.lower() for word in tweets_string.split(' ')]
+    	total_word_count = len(tweets_words)
 
     	stop_words = sw.get_stop_words('english')
     	# change encoding scheme from unicode to utf-8 to match tweets
     	stop_words = [stop_word.encode('utf-8') for stop_word in stop_words]
+    	stop_words += ['', '@', '-']
     	
     	# filter out stop words
     	tweets_words = [word for word in tweets_words if word not in stop_words]
+
+    	word_freq_dict = {}
+    	for word in tweets_words:
+    		if word in word_freq_dict.keys():
+    			word_freq_dict[word] += 1
+    		else:
+    			word_freq_dict[word] = 1
+
+    	# sort based on the frequency count (descending order)
+    	sorted_word_freq = sorted(word_freq_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+    	for word_tuple in sorted_word_freq[:10]:
+    	 	print word_tuple[0]
